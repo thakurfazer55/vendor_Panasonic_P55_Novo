5e09882b1cf40ab8ada36005873a8953f5686e9e
brownietheboy <brownietheboy@gmail.com>
2017-05-31 18:05:47 +0530

    first commit

---
+++ P55_Novo-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55_Novo/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55_Novo/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55_Novo/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55_Novo/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Panasonic/P55_Novo/proprietary/vendor,system/vendor)
